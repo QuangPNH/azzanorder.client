3662d101a3cb9e040d9d45751cb02e82345d4a65
somethingtho <116186964+somethingtho@users.noreply.github.com>
2024-12-13 21:05:10 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -70,7 +70,7 @@ const tableqrParam = searchParams.get("tableqr");
 const tableqrCookie = getCookie("tableqr");
 
 // Check if both tableqrParam and tableqrCookie are missing
-if (!tableqrParam && !tableqrCookie) {
+if (!tableqrParam || !tableqrCookie) {
     // Redirect to the provided link
     window.location.href = "https://manager-owner-bhcrh9dvahbkgpfu.southeastasia-01.azurewebsites.net/";
 } else {
