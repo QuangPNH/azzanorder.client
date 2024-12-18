8bd75a028239e0943b54dced5db784d35b699ccf
QuangPNH <quangpnhhe170627@fpt.edu.vn>
2024-12-19 01:24:22 +0700

    modify api url

--- src/config/apiUrls.js
+++ src/config/apiUrls.js
@@ -1,6 +1,6 @@
 // src/config/apiUrls.js
 const API_URLS = {
-    API: "https://oas-main-api-cwf5hnd9apbhgnhn.southeastasia-01.azurewebsites.net/api/",
+    API: "https://push-noti-api-amg8fwasfebchtf2.southeastasia-01.azurewebsites.net/api/",
     PAYOS: "https://payos-main-e7aybvchbkgsb9em.southeastasia-01.azurewebsites.net/",
     NOTHUB: "https://push-noti-api-amg8fwasfebchtf2.southeastasia-01.azurewebsites.net/"
     // Add other endpoints as needed
