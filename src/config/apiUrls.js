13b4c7976db7fb6c936f7614021ea35859a06d49
Vipxpert <vipxpert3721@gmail.com>
2025-01-02 22:01:21 +0700

    Update config links

--- src/config/apiUrls.js
+++ src/config/apiUrls.js
@@ -1,8 +1,8 @@
 // src/config/apiUrls.js
 const API_URLS = {
-    API: "https://oas-api-main-cxhua5dqh7dueqhz.southeastasia-01.azurewebsites.net/api/",
-    PAYOS: "https://payos-main-e7aybvchbkgsb9em.southeastasia-01.azurewebsites.net/",
-    NOTHUB: "https://push-noti-api-amg8fwasfebchtf2.southeastasia-01.azurewebsites.net/"
+    API: "https://oas-main-api-cwf5hnd9apbhgnhn.southeastasia-01.azurewebsites.net/api/",
+    PAYOS: "https://payos-handling-d5egafdqeqh7e5dg.southeastasia-01.azurewebsites.net/",
+    NOTHUB: "https://pushnotiapi-h3dkf2f6h6f5cbcj.southeastasia-01.azurewebsites.net/"
     // Add other endpoints as needed
   };
   
